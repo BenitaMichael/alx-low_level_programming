7fa53c6ef82be8549dc967f59aea9472f9e9c39d
BenitaMichael <benubiaka@gmail>
2023-03-17 14:19:50 -0700

    Task 10 : program that prints all possible different combinations of two digits.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,7 +2,7 @@
 /**
  *main - Entry point
  *Description: printing all possible different combinations of two digits
- *Return: ALways 0 (successful)
+ *Return: ALways 0 (success)
  */
 int main(void)
 {
@@ -11,7 +11,7 @@ int main(void)
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		for (j = 1 ; i < 10 ; j++)
+		for (j = 1 ; j < 10 ; j++)
 		{
 			if (i < j && i != j)
 			{
@@ -19,11 +19,12 @@ int main(void)
 				putchar(j + '0');
 				if (i + j != 17)
 				{
-					putchar (',');
+					putchar(',');
 					putchar(' ');
 				}
 			}
 		}
+
 	}
 	putchar('\n');
 	return (0);
