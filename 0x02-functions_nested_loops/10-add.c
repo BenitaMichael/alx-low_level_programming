e13f1888149239e0ddce2844b8fd8823327f92bb
BenitaMichael <benubiaka@gmail>
2023-03-21 14:38:46 -0700

    Task 10 - adding numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *add - add two integers
+ *@x: The first number to be entered
+ *@y: The second number to be added
+ *Return: Integer value;
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
