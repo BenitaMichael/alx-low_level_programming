4d2790d763562dd492223feeaa4e6a8b09ca6829
BenitaMichael <benubiaka@gmail>
2023-03-21 08:40:25 -0700

    Task 2 - function that prints alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - function to print alphabets a - z 10 times
+ *Description: printing alphabet in lowecase 10 times
+ */
+
+print_alphabet_x10(void)
+{
+	int i = 0;
+	char letter;
+
+	while (i < 11)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+		_putchar('\n');
+		i++;
+	}
+}
