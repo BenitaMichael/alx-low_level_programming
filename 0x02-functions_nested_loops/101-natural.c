109ce150f754197740104026932ded2b6ebe8f28
BenitaMichael <benubiaka@gmail>
2023-03-21 16:50:07 -0700

    Task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main _ prints all multiples of 3 and 5
+ *Return: Always 0 (successful);
+ */
+
+int main(void)
+{
+	int i, j = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			j += i;
+		}
+		i++;
+	}
+	printf("%d\n", j);
+	return (0);
+}
