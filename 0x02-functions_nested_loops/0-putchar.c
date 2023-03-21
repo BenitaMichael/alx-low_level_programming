0337ae012b5f3e0f477f745dd6dde172411b5a6e
BenitaMichael <benubiaka@gmail>
2023-03-21 04:50:25 -0700

    Task 1 - writing a program that prints putchar\

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio>
+/**
+ *main - Entry point
+ *Description: print _putchar in stdout
+ *Return: Always 0 (successful)
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
