5754b7361cecafeafa98214b8536b983cc2793fb
BenitaMichael <benubiaka@gmail>
2023-03-21 07:05:25 -0700

    Task 1 - printing alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_alphabet - function to print alphabets a - z
+ *Description: printing alphabet in lowecase
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n);
+}
