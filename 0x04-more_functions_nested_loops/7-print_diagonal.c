4d90ad7a5086577ddd462d695356d25422fb40d2
BenitaMichael <benubiaka@gmail>
2023-03-23 06:05:58 -0700

    ;Task 7 - print diagonal lines

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,10 +18,10 @@ void print_diagonal(int n)
 		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < n; j++)
+			{
 				if (j == i)
 					_putchar('\\');
 				else if (j > i)
-				{
 					_putchar(' ');
 			}
 			_putchar('\n');
