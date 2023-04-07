d144e3baa60b5659181a455c8e8f8535d7d9f8c5
BenitaMichael <benubiaka@gmail>
2023-04-07 14:40:18 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point - prints name of file follow
+ * by new line
+ * @argc: number of command line arguments
+ * @argv: array that contains the program command line arguments
+ * Return: (0) if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
