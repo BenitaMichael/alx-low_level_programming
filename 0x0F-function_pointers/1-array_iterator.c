a9b9a0c5ffc14452477e6bf8f31a9cd725a6d21e
BenitaMichael <benubiaka@gmail>
2023-04-19 10:31:29 -0700

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,7 +4,7 @@
 /**
  * array_iterator - executes a function given as a
  * parameter on each element of an array
- * @array: input integer array
+ * @array: input inteeger array
  * @size: size of the array
  * @action: pointer to the function
  * Return: no return
@@ -16,5 +16,5 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array && action)
 		for (i = 0; i < size; i++)
-			array[i];
+			action(array[i]);
 }
