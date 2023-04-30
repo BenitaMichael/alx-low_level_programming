6baa35ce1726c7dea1dfd3561120f37a083af443
BenitaMichael <benubiaka@gmail.com>
2023-04-30 13:18:22 -0700

    Task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a list
+ * @h: head of a list
+ * Return: numbers of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t no_nodes = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		no_node++;
+	}
+
+	return(no_nodes);
+}
