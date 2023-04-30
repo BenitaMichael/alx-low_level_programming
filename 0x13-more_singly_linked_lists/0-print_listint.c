8643a749de466680fcd841d488d4f93db4e35985
BenitaMichael <benubiaka@gmail.com>
2023-04-30 13:21:42 -0700

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,10 +12,10 @@ size_t print_listint(const listint_t *h)
 
 	while (h)
 	{
-		printf("%d\n", h->n);
+		printf("%d\n", h->no);
 		h = h->next;
-		no_node++;
+		no_nodes++;
 	}
 
-	return(no_nodes);
+	return (no_nodes);
 }
