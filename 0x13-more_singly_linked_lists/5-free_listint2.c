926c71f86d832ea9497146d9ac7a74bb0416fc80
BenitaMichael <benubiaka@gmail.com>
2023-04-30 15:59:24 -0700

    Task 5 - I am tired

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+* free_listint2 - function that frees a listint_t list
+* sets the head to NULL
+* @head: pointer to pointer to head node
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		while (*head)
+		{
+			temp = *head;
+			*head = temp->next;
+			free(temp);
+		}
+	}
+}
